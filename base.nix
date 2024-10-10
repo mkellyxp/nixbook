5cf71a0984ff3d6c40a35dd54e0a65015923493a
Mike Kelly <mike@membervault.co>
2024-10-10 11:10:06 -0700

    auto update

--- base.nix
+++ base.nix
@@ -1,4 +1,3 @@
-
 { config, pkgs, ... }:
 {
   # Enable the X11 windowing system.
@@ -25,35 +24,7 @@
 
   services.flatpak.enable = true;
 
-  systemd.timers."auto-updater" = {
-    wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnBootSec = "5m";
-        OnUnitActiveSec = "5m";
-        Unit = "auto-updater.service";
-        Persistent = true;
-        AccuracySec = "1month";
-      };
-  };
-
-
-  systemd.services."auto-updater" = {
-    script = ''
-      set -eu
-      ${pkgs.coreutils}/bin/touch "/home/user/$(date +'%Y%m%d%H%M%S').txt"
-
-      ${pkgs.nix}/bin/nix-channel --update
-      # ${pkgs.nixos-rebuild}/bin/nixos-rebuild boot --upgrade
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-    };
-    wantedBy = [ "multi-user.target" ]; # Ensure the service starts after rebuild
-  };
-
-
-
-
-
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.channel = "https://nixos.org/channels/nixos-24.05";
+  # system.autoUpgrade.schedule = "daily";
 }
