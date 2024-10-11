5d2de90221d8933af986cfdd69b28e32a8b9b97e
Michael Kelly <mike@thegrumpydev.com>
2024-10-11 14:40:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-cp /etc/nixbook/config/config ~/.config
+cp -R /etc/nixbook/config/config ~/.config
+cp -R /etc/nixbook/config/local ~/.local
+
+sudo sed -i '/hardware-configuration\.nix/a\      /etc/nixbook/base.nix' /etc/nixos/configuration.nix
+sudo nixos-rebuild switch
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
