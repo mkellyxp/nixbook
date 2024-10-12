f2cd008ed5bcc4fcf722affd9ccc255974615c77
Michael Kelly <mike@thegrumpydev.com>
2024-10-11 20:00:28 -0700

    Create powerwash.sh

---
+++ powerwash.sh
@@ -0,0 +1,11 @@
+read -p "This will delete ALL local files and reset this Nixbook!  Do you want to continue? (y/n): " answer
+
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+echo "Powerwashing NixBook..."
+  sudo rm -rf ~/*
+  cp -R /etc/nixbook/config/config ~/.config
+  cp -R /etc/nixbook/config/local ~/.local
+  reboot
+else
+  echo "Powerwashing Cancelled!"
+fi
