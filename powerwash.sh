cc5aa6d2e050cddab97786e18e8ca3781d16ac7c
Michael Kelly <mike@thegrumpydev.com>
2024-10-15 07:27:51 -0700

    updated for clearing space and home directories

--- powerwash.sh
+++ powerwash.sh
@@ -1,11 +1,21 @@
-read -p "This will delete ALL local files and reset this Nixbook!  Do you want to continue? (y/n): " answer
+read -p "This will delete ALL local files and reset this Nixbook!\nDo you want to continue? (y/n): " answer
 
 if [[ "$answer" =~ ^[Yy]$ ]]; then
 echo "Powerwashing NixBook..."
+  # Get latest nixbook code
   sudo git -C /etc/nixbook pull
+
+  # Clear space and rebuild
+  sudo nix-collect-garbage -d
   sudo nixos-rebuild boot --upgrade
+
+  # Erase data and set up home directory again
   rm -rf ~/
+  mkdir ~/Desktop
+  mkdir ~/Documents
+  mkdir ~/Downloads
   cp -R /etc/nixbook/config/config ~/.config
+  
   reboot
 else
   echo "Powerwashing Cancelled!"
