80e02c1dc6066d10bda21b3507ba8491b295042f
Michael Kelly <mike@thegrumpydev.com>
2024-10-18 13:31:50 -0700

    depth 1

--- base_lite.nix
+++ base_lite.nix
@@ -43,7 +43,7 @@
   systemd.services."auto-update-config" = {
     script = ''
       set -eu
-      ${pkgs.git}/bin/git -C /etc/nixbook pull
+      ${pkgs.git}/bin/git -C /etc/nixbook pull --depth 1
     '';
     serviceConfig = {
       Type = "oneshot";
