55f2f7066468cfbe461a55ca4f1a06a7eab506de
phillipzink <40325165+phillipzink@users.noreply.github.com>
2024-12-18 14:26:59 -0500

    Update update.sh
    Download the latest nixbook changes
    Update the nixos nix-channel to be the same as config.system.autoUpgrade.channel in base.nix or base_lite.nix
    Free up space before updates
    Free up a little more space with hard links

--- update.sh
+++ update.sh
@@ -3,8 +3,24 @@ read -p "Do you want to continue? (y/n): " answer
 
 if [[ "$answer" =~ ^[Yy]$ ]]; then
   echo "Updating Nixbook..."
-  sudo nixos-rebuild boot --upgrade
+
+  # Download the latest nixbook changes
+  sudo git -C /etc/nixbook pull
+
+  # Update the nixos nix-channel to be the same as config.system.autoUpgrade.channel in base.nix or base_lite.nix
+  AUTOUPDATE_CHANNEL="$(sudo nix-instantiate --eval '<nixpkgs/nixos>' -A config.system.autoUpgrade.channel | tr -d '"')"
+  sudo nix-channel --add "${AUTOUPDATE_CHANNEL}" nixos
+
+  # Free up space before updates
+  nix-collect-garbage --delete-older-than 30d
+
+  # get the updates
   flatpak update -y
+  sudo nixos-rebuild boot --upgrade
+
+  # free up a little more space with hard links
+  nix-store --optimise
+
   reboot
 else
   echo "Update Cancelled!"
