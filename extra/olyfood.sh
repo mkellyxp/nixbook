0f21f02ae720ed96a7b770ae06c02817ff710d5a
Michael Kelly <mike@membervault.co>
2025-08-17 18:52:14 -0700

    Creates desktop shortcuts for food resources
    Adds a script to create desktop shortcuts for accessing
    online resources related to food assistance programs and
    community initiatives, enhancing user accessibility.

--- extra/olyfood.sh
+++ extra/olyfood.sh
@@ -12,3 +12,35 @@ EOL
 
 # Make it executable
 chmod +x "$DESKTOP_FILE"
+
+
+DESKTOP_FILE="$HOME/Desktop/LOCAL_OLYMPIA_FREE_FOOD_RESOURCES.desktop"
+
+# Create the .desktop file
+cat > "$DESKTOP_FILE" <<EOL
+[Desktop Entry]
+Version=1.0
+Type=Link
+Name=LOCAL OLYMPIA FREE FOOD RESOURCES
+URL=https://foodsourcefoundation.org/resources/
+Icon=text-html
+EOL
+
+# Make it executable
+chmod +x "$DESKTOP_FILE"
+
+
+DESKTOP_FILE="$HOME/Desktop/START_A_GROUP.desktop"
+
+# Create the .desktop file
+cat > "$DESKTOP_FILE" <<EOL
+[Desktop Entry]
+Version=1.0
+Type=Link
+Name=START A GROUP
+URL=https://soupersundayolympia.com/start-a-group/
+Icon=text-html
+EOL
+
+# Make it executable
+chmod +x "$DESKTOP_FILE"
