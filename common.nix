9498d3e21963592f256679afb117136cf39e2184
Michael Kelly <mike@membervault.co>
2025-12-06 15:07:46 -0800

    Enables common VM guest additions
    Configures services and settings for common VM guest additions to improve host-guest interaction in virtualized environments.
    
    Explicitly disables VirtualBox guest additions due to potential kernel module compatibility issues.
    
    Relates to #2511

--- common.nix
+++ common.nix
@@ -29,6 +29,18 @@
     "flakes"
   ];
 
+  # VM guest additions to improve host-guest interaction
+  services.spice-vdagentd.enable = true;
+  services.qemuGuest.enable = true;
+  virtualisation.vmware.guest.enable = pkgs.stdenv.hostPlatform.isx86;
+  # https://github.com/torvalds/linux/blob/00b827f0cffa50abb6773ad4c34f4cd909dae1c8/drivers/hv/Kconfig#L7-L8
+  virtualisation.hypervGuest.enable =
+    pkgs.stdenv.hostPlatform.isx86 || pkgs.stdenv.hostPlatform.isAarch64;
+  services.xe-guest-utilities.enable = pkgs.stdenv.hostPlatform.isx86;
+  # The VirtualBox guest additions rely on an out-of-tree kernel module
+  # which lags behind kernel releases, potentially causing broken builds.
+  virtualisation.virtualbox.guest.enable = false;
+
   # Common Packages
   environment.systemPackages = with pkgs; [
     git
