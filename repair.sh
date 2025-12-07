90eb4b0860c28c7c7138b7bc4a0976fa2044ff24
Michael Kelly <mike@thegrumpydev.com>
2025-12-07 12:45:03 -0800

    Fixes flatpak update issues
    Addresses a rare issue where flatpak updates fail due to corrupted files.
    
    Ensures Nixbook stays on the correct channel.

--- repair.sh
+++ repair.sh
@@ -2,14 +2,8 @@
 # It is purposely in a script in case we need to fix something that is breaking nix rebuilds
 
 # Fixes rare, but annoying issue with flatpak updates failing due to corrupt files.
-sudo flatpak repair --system
-sudo flatpak update -y
- 
-# Will make sure Nixbook is on the correct channel
-currentChannel=$(sudo nix-channel --list | grep '^nixos' | awk '{print $2}')
-targetChannel="https://nixos.org/channels/nixos-25.11"
 
-if [ "$currentChannel" != "$targetChannel" ]; then
-  sudo nix-channel --add "$targetChannel" nixos
-  sudo nix-channel --update
-fi
+/run/current-system/sw/bin/nix-channel --add https://nixos.org/channels/nixos-25.11 nixos
+
+# sudo flatpak repair --system
+# sudo flatpak update -y
